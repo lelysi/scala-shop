e82290e3950d8f29bbf36d61a2f5d3db5ad730f6
lelysi <denis.makovey@gmail.com>
2019-11-24 19:55:59 +0100

    registration feature. add incorrect input json test case

--- src/test/scala/lelysi/scalashop/functional/UserRegisterSpec.scala
+++ src/test/scala/lelysi/scalashop/functional/UserRegisterSpec.scala
@@ -1,6 +1,7 @@
 package lelysi.scalashop.functional
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
+import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import lelysi.scalashop.{JsonSupport, ShopRoute}
 import org.scalatest._
@@ -11,15 +12,29 @@ class UserRegisterSpec extends WordSpec
   with JsonSupport
   with ShopRoute {
 
-  val entity = HttpEntity(
+  val url: String = "/registration"
+
+  lazy val correctEntity = HttpEntity(
     ContentTypes.`application/json`,
     """{ "email": "example@example.com", "password" : "pass" }"""
   )
 
+  lazy val incorrectEntity = HttpEntity(
+    ContentTypes.`application/json`,
+    """{ "password" : "pass" }"""
+  )
+
   "User Register Service" should {
     "return 200 for correct register data" in {
-      Post("/registration", entity) ~> userRegistration ~> check {
+      Post(url, correctEntity) ~> userRegistration ~> check {
+        status shouldEqual StatusCodes.OK
         responseAs[String] shouldEqual "New user with email example@example.com was registered"
+      }
+    }
+
+    "return 400 for incorrect register data" in {
+      Post(url, incorrectEntity) ~> Route.seal(userRegistration) ~> check {
+        status shouldEqual StatusCodes.BadRequest
       }
     }
   }
