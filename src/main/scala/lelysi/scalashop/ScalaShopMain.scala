b4589ce468473b024d99d8f24a4240e5b49eb882
lelysi <denis.makovey@gmail.com>
2019-11-25 19:54:11 +0100

    add config

--- src/main/scala/lelysi/scalashop/ScalaShopMain.scala
+++ src/main/scala/lelysi/scalashop/ScalaShopMain.scala
@@ -5,18 +5,21 @@ import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 
 object ScalaShopMain extends App
   with JsonSupport
   with ShopRoute {
 
+  val config = ConfigFactory.load()
+  val host = config.getString("http.host")
+  val port = config.getInt("http.port")
+
   implicit val system: ActorSystem = ActorSystem("scala-shop")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val log: LoggingAdapter = Logging(system, "main")
 
-  val port = 8080
-
-  val bindingFuture = Http().bindAndHandle(Route.handlerFlow(userRegistration), "localhost", port)
+  val bindingFuture = Http().bindAndHandle(Route.handlerFlow(userRegistration), host, port)
 
   log.info(s"Server started at the port $port")
 }
